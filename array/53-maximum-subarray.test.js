17385c6161077f6e17c50776bdab62c7deee041e
Daniele Tortora <contact@danieletortora.com>
2022-05-23 14:33:06 +0200

    53 maximum subarray

---
+++ array/53-maximum-subarray.test.js
@@ -0,0 +1,55 @@
+// https://leetcode.com/problems/maximum-subarray/
+
+// Given an integer array nums, find the contiguous subarray(containing at least one number) which has the largest sum and return its sum.
+
+// A subarray is a contiguous part of an array.
+
+/**
+ * O(N^2) brute force solution
+ */
+var maxSubArray_brute = function (nums) {
+  if (nums.length < 2) return nums[0] || 0;
+
+  let sum = -Infinity;
+
+  for (let i = 0; i < nums.length; i++) {
+    let sub = 0;
+
+    for (let j = i; j < nums.length; j++) {
+      sub += nums[j];
+
+      sum = Math.max(sum, sub);
+    }
+  }
+
+  return sum;
+};
+
+/**
+ * Approach 2: Dynamic Programming, Kadane's Algorithm
+ *
+ */
+
+// set initials
+// 1.1 first value in the array. It doesn’t matter to us whether max is positive or negative.
+// 1.2 we need an intermediate current positive value, because a negative value does not interest us, since -2 is always more than -5 and it means 0 + -2 > -5.
+// At this point we've covered first value in the array, that's why we will move through array starting from the 1 index.
+// current += nums[i] means that every time we take a new value, we will sum it with the past current value. This will help us discard lower values in the next step.
+// max = Math.max(max, current) means that we choose only the highest value (previous max or new current).
+// current = Math.max(current, 0) means that if the value was negative, then we throw it away and set zero as default. Why? Because it makes no sense to summarize the negative values, they simply do not interest us. The previous step is enough to compare negative values. There we will choose the minimum negative value (if it is the case i.e [-10, -3, -5, -2, -6] will return only -2 because it is the highest value from the negative ones.
+const maxSubArray = (nums) => {
+  let max = nums[0];
+  let current = Math.max(max, 0);
+
+  for (let i = 1; i < nums.length; i += 1) {
+    current += nums[i];
+    max = Math.max(max, current);
+    current = Math.max(current, 0);
+  }
+
+  return max;
+};
+
+test("maxSubArray", () => {
+  expect(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])).toBe(6);
+});
--- tree/235-lowest-common-ancestor-of-bst.test.js
+++ tree/235-lowest-common-ancestor-of-bst.test.js
@@ -6,6 +6,9 @@ import { convertArrayToTree } from "./utils/transformer";
 
 // According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
 
+/**
+ * O(H) where H is the height of the tree, which translates to O(log N)
+ */
 var lowestCommonAncestor = function (root, p, q) {
   const dfs = (node) => {
     if (!node) return null;
