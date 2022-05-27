118a33a7f521d2aff26dc3b5ddaaab6f0e142dd4
Daniele Tortora <contact@danieletortora.com>
2022-05-27 12:54:05 +0200

    252 meeting rooms

---
+++ intervals/252-meeting-rooms.test.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/meeting-rooms/
+
+/**
+ * The strategy here is to SORT the meeting by START TIME,
+ * and then COMPARE LAST END with NEXT START
+ *
+ * IF LAST END > NEXT START - then meeting cannot be attended
+ */
+
+/**
+ * @param {number[][]} intervals
+ * @return {boolean}
+ */
+var canAttendMeetings = function (intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  let lastEnd;
+
+  for (let [start, end] of intervals) {
+    // this is the first meeting we're looking at
+    if (!lastEnd) {
+      lastEnd = end;
+      continue;
+    }
+
+    if (start < lastEnd) return false;
+    lastEnd = end;
+  }
+
+  return true;
+};
