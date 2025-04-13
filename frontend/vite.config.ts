1b891f857cefd0b18c5cdc5c2b20f15d2cf266c1
Daniele Tortora <45793869+floroz@users.noreply.github.com>
2025-04-13 13:08:13 +0200

    feat: add react compiler

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwind from "@tailwindcss/vite";
 
+const ReactCompilerConfig = {
+  target: "19",
+};
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwind(), react()],
+  plugins: [
+    tailwind(),
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+  ],
 });
